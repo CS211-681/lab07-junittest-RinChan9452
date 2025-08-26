2440abc508c3b906ed696bbacabbbf390853b05f
RinChan <nattapol.taw@ku.th>
2025-08-26 19:02:31 +0700

    start test Student.java and StudentList.java

---
+++ src/test/java/ku/cs/models/StudentListTest.java
@@ -0,0 +1,13 @@
+package ku.cs.models;
+
+import org.junit.jupiter.api.Test;
+import ku.cs.services.StudentListHardCodeDatasource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentListTest {
+    @Test
+    void testFindStudentById() {
+
+    }
+}
---
+++ src/test/java/ku/cs/models/StudentTest.java
@@ -0,0 +1,46 @@
+package ku.cs.models;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    @DisplayName("ทดสอบการเพิ่มคะแนน 45.15 คะแนน")
+    void testAddScore(){
+        Student s = new Student("6xxxxxxxx", "StudentTest");
+        s.addScore(45.15);
+        assertEquals(45.15, s.getScore());
+
+    }
+
+    @Test
+    @DisplayName("ทดสอบการเพิ่มคะแนน 85 คะแนน และให้ Object คำนวนเกรดออกมา")
+    void testCalculateGrade(){
+        Student s = new Student("6xxxxxxxxx", "StudentTest");
+        s.addScore(85);
+        assertEquals("A", s.grade());
+    }
+
+    @Test
+    void testGetScore(){
+        Student s = new Student("6xxxxxxxx", "StudentTest");
+        s.addScore(45.15);
+        assertEquals(45.15, s.getScore());
+    }
+
+  @Test
+    void testIsId(){
+        Student s = new Student("6xxxxxxxx", "StudentTest");
+        assertTrue(s.isId("6xxxxxxxx"));
+  }
+
+  @Test
+    void testIsNameContain(){
+        Student s = new Student("6xxxxxxxx", "StudentTest");
+        assertTrue(s.isNameContains("King Joe"));
+  }
+
+}
